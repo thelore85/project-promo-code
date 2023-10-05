6902611bd9a04ca5b2acb2291db4a24f6ccd267e
TheLore85 <garofalo.digital@gmail.com>
2023-10-05 16:39:44 +0200

    adding full design and legal pages v2

--- app/Components/Form/Form.js
+++ app/Components/Form/Form.js
@@ -4,6 +4,31 @@ import React, {useState} from 'react';
 import './Form.css'
 import '../Server/Server.js'
 
+/////////////////////////////////////////
+// ------ my app - SERVER CONNECTION
+////////////////////////////////////////
+
+export let serverUrl;
+const serverPort = 9000;  // use the same port of the server
+const serverPath = '/project-promo-code' // place here your project path
+const serverAdress = 'https://server-piqus.vercel.app';
+const serverLive = `${serverAdress}${serverPath}`;
+const serverLocal = `http://localhost:${serverPort}${serverPath}`
+
+
+// server url builder
+const  appEnv = process.env.NODE_ENV || 'development';
+
+const serverUrlbuilder = () => {
+  if(appEnv === 'development' ){
+    serverUrl = serverLocal
+  }else{
+    serverUrl = serverLive
+  }
+};
+serverUrlbuilder();
+
+
 
 /////////////////////////////////////////
 //// COMPONENT FUNCTION
