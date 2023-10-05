232ec64ad0bcb2519e13b48b7e87af4c816eef7d
TheLore85 <garofalo.digital@gmail.com>
2023-10-05 16:47:29 +0200

    adding full design and legal pages v5

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 
 import Hero from '@/app/Components/Hero/Hero.js'
-import Main from '../Components/Main/Main.js'
-import Form from '../Components/Form/Form.js'
-import Footer from '../Components/Footer/Footer.js'
-import Carousel from '../Components/Carousel/Carousel.js'
+import Main from './Components/Main/Main.js'
+import Form from './Components/Form/Form.js'
+import Footer from './Components/Footer/Footer.js'
+import Carousel from './Components/Carousel/Carousel.js'
 
 export default function Home() {
   return (
