7719ed2a25509ca9fee5c7f236c1a5ae3ea35de8
TheLore85 <garofalo.digital@gmail.com>
2023-10-05 16:52:47 +0200

    adding full design and legal pages v6

--- app/termini/page.js
+++ app/termini/page.js
@@ -11,14 +11,14 @@ export default function termini() {
       <ul>
           <li><strong>Titolare del Sito (Il Titolare):</strong> Rappresentato dalla partita IVA grflnz85c26f205o, il Titolare offre il servizio di creazione e gestione di landing page per la raccolta di dati di contatto degli Utenti.</li>
           <li><strong>Il Brand/Servizio/Cliente Sponsorizzato (Il Brand/Servizio/Cliente):</strong> Il soggetto che sponsorizza la landing page e riceve i dati di contatto raccolti dagli Utenti.</li>
-          <li><strong>L'Utente del Sito:</strong> Gli individui che interagiscono con la landing page e forniscono i propri dati di contatto in cambio di un codice sconto o di un'altra offerta.</li>
+          <li><strong>L&#39Utente del Sito:</strong> Gli individui che interagiscono con la landing page e forniscono i propri dati di contatto in cambio di un codice sconto o di un&#39altra offerta.</li>
       </ul>
   
-      <h2>2. Oggetto del Servizio ("Your Personal Promo")</h2>
-      <p>Il Titolare offre una tecnologia proprietaria che permette di creare landing page in grado di raccogliere i dati di contatto degli Utenti. In cambio, gli Utenti riceveranno un codice sconto o un'offerta personalizzata, studiata ad hoc per il Cliente.</p>
+      <h2>2. Oggetto del Servizio (&#34Your Personal Promo&#34)</h2>
+      <p>Il Titolare offre una tecnologia proprietaria che permette di creare landing page in grado di raccogliere i dati di contatto degli Utenti. In cambio, gli Utenti riceveranno un codice sconto o un&#39offerta personalizzata, studiata ad hoc per il Cliente.</p>
   
-      <h2>3. Gestione dei Dati dell'Utente</h2>
-      <p>I dati degli Utenti verranno archiviati in un CRM partner del Titolare, secondo le normative sulla privacy vigenti. Questi dati saranno marchiati con un'etichetta riconducibile al Cliente, ma saranno di proprietà esclusiva del Titolare. Il Cliente non avrà accesso diretto ai dati degli Utenti ma potrà richiedere al Titolare l'attivazione e la gestione degli stessi per finalità commerciali specifiche.</p>
+      <h2>3. Gestione dei Dati dell&#39Utente</h2>
+      <p>I dati degli Utenti verranno archiviati in un CRM partner del Titolare, secondo le normative sulla privacy vigenti. Questi dati saranno marchiati con un&#39etichetta riconducibile al Cliente, ma saranno di proprietà esclusiva del Titolare. Il Cliente non avrà accesso diretto ai dati degli Utenti ma potrà richiedere al Titolare l&#39attivazione e la gestione degli stessi per finalità commerciali specifiche.</p>
   
       <h2>4. Archiviazione e Normative sulla Privacy</h2>
       <p>Il Titolare si impegna a archiviare i dati degli Utenti in conformità alle normative sulla privacy vigenti. I dati verranno gestiti in modo sicuro e protetto.</p>
@@ -27,13 +27,13 @@ export default function termini() {
       <p>Il Titolare offre anche un servizio di comunicazione online, incluso lo sviluppo della landing page.</p>
   
       <h2>6. Raccolta dei Dati di Contatto</h2>
-      <p>La landing page contiene un form in cui gli Utenti possono inserire il proprio nome e indirizzo email in cambio di un codice sconto, che verrà inviato tramite email e potrà essere utilizzato alla cassa il giorno dell'inaugurazione.</p>
+      <p>La landing page contiene un form in cui gli Utenti possono inserire il proprio nome e indirizzo email in cambio di un codice sconto, che verrà inviato tramite email e potrà essere utilizzato alla cassa il giorno dell&#39inaugurazione.</p>
   
       <h2>7. Consenso Informato</h2>
-      <p>La raccolta dei dati di contatto avviene con il consenso informato degli Utenti, espresso durante la compilazione del form. Gli Utenti sono informati sull'uso dei loro dati e sui diritti di cui dispongono.</p>
+      <p>La raccolta dei dati di contatto avviene con il consenso informato degli Utenti, espresso durante la compilazione del form. Gli Utenti sono informati sull&#39uso dei loro dati e sui diritti di cui dispongono.</p>
   
       <h2>8. Responsabilità del Titolare e del Cliente Sponsorizzato</h2>
-      <p>Il Cliente Sponsorizzato non è responsabile della gestione diretta dei dati raccolti ma può richiedere al Titolare l'attivazione e l'utilizzo degli stessi per scopi commerciali specifici.</p>
+      <p>Il Cliente Sponsorizzato non è responsabile della gestione diretta dei dati raccolti ma può richiedere al Titolare l&#39attivazione e l&#39utilizzo degli stessi per scopi commerciali specifici.</p>
   
       <p>Per ulteriori informazioni sulla gestione dei dati, la privacy e il servizio offerto, si prega di contattare il Titolare del Sito.</p>
   
