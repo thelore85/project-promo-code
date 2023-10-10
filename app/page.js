ded23227e66e67f5a6e665f7616fb4df0da31c26
TheLore85 <garofalo.digital@gmail.com>
2023-10-10 23:50:15 +0200

    contratto

--- app/page.js
+++ app/page.js
@@ -22,3 +22,157 @@ export default function Home() {
 //         </>
 //   )
 // }
+
+
+
+///////////////////////////////////////
+// TO DO LIST: DEVELOPING FEATURE
+
+
+// - DB: project_promo_user (userId - email - psw -  company - tear - contratto)
+// - DB: project_promo_code (promoId - email - promoCode - promo start - promo end)
+// - DB: project_promo_service (serviceId - email - service(eg: landing, blog, ...) - prezzo -  )
+
+
+// - HS: verificare come 'pushare' i dati del db dentro HS in maniera automatica.
+// - HS: aggiungere campi contatto: citta' - settore - tipologia (consumatore, info, ...)
+// - APP: login feature (psw encripted, recupero psw, )
+
+
+
+
+
+
+
+///////////////////////////////////
+///////////////////////////////////
+// ----------------------------- //
+// CONTRATTO SERVIZIO PIQUS LEAD //
+///////////////////////////////////
+///////////////////////////////////
+
+
+
+
+
+
+////////////////////////////////
+// PARTI COINVOLTE
+
+// - Il titolare: proprietario della landing page e della piattaforma
+// - il cliente: brand o servizio sponsorizzato, sottoscrittore del contratto.
+// - Gli utenti: visitatori della landing page
+// - lead o contatti: un utente che compila il form di contatto
+// - il servizio: 'piqus lead'
+
+
+///////////////////////////////////////////
+// SOTTOSCRIZIONE DEL CONTATTO
+
+// - il seguente contratto regola il rapporto tra il titolare e il cliente in merito al servizio 'piqus lead'
+// - il presente contratto e' sottoposto in fase di creazione dell'account 'piqus lied' attraverso la piattaforma raggiungibile dal sito 'lead.piqus.it'
+// - attivando l'account, si considera sottoscritto il presente contratto.
+// - il presente contratto ha validita' di 12 mesi e si rinnovera' automaticamebte per mantenere continuita' di servizio. Il cliente puo' interrompere la sua efficacia in qualsiasi momento inviando una mail a supporto@piqus.it o direttamebte dalla piattaforma nel pannello di amministrazione.
+
+
+///////////////////////////////////////
+// DESCRIZIONE DEL SERVIZIO 'piqus lead'
+
+// creazione di landing page o strumenti web finalizzati alla generazione di contatti (lead).
+
+// modello di servizio 'a perfomance': quanto piu' il cliente genera lead, tanto piu' ha diritto ai servizi descritti nel tear di riferimento come descritto nel seguente contratto
+// il modello di servizio si divide in 4 livelli: 'tear light', 'tear basic', 'tear premium', 'tear platinum';
+// Non e' possibile sottoscriversi a un 'tear' pagando. pertanto non sono richiesti dati di pagamento o fatturazione per la generazione dell'account.
+// il servizio 'Piqus lead' si attiva semplicemente con la creazione di un account dal sito 'www.lead.piqus.it';
+// al cliente viene attribuito il 'tear: light' di default;
+// il cliente verra' contattato direttamente per raccogliere le informazioni necessarie a ceare il materiale porposto nel 'tear: light';
+
+// Per completezza viene inseirto a valle di questo contratto il listino per i servizi offerti in modo indipendente.
+// E' possibile altresi' richiedere servizi specifici a pagamento. in questo caso il titolare gestira' in maniera indipendente la richiesta che quindi esula da questo contratto.
+
+
+
+//////////////////////////////////////
+// GESTIONE DEI LEAD GENERATI - CRM (Customer Relationship Management)
+
+// -  i dati dei contatti verranno salvati in un crm partner del titolare. i dati sono di proprieta' del titolare. 
+// -  i contatti nel crm sono divisi per liste associate ai vari clienti del titolare. Il titolare garantisce l'assegnazione del lead del cliente alla sua lista privata.
+// -  il cliente non puo' disporre direttamente dei dati dei contatti, ma puo' ingaggiare il titolare per azioni di marketing sulla lista a lui dedicata, secondo le modalita' indicate nel presente acccordo.
+// -  Il cliente non potra' richiedere manipolazioni o elaborazioni dei dati associata alla sua lista.
+// -  in caso di scadenza o mancato rinnovo del contratto di sottoscrizione, i lead sono e rimangono a disposizione del titolare 
+
+
+
+
+/////////////////////////////////////////////
+// LIGHT TEAR - up to 500 lead
+
+// -  1 landing page: design, sviluppo e messa online gratis della prima landing page promozionale. Versione standard (sezioni della pagina: header, promo message, form, footer)
+// -  1 mail automatica: personalizzazione mail di risposta all'utente
+// -  1 invio mail/anno: verso la lista contatti generata
+// -  pannello admin: accesso al pannello di amministrazione
+// -  account - supporto limitato alla consulenza cretiva per i materiali in elenco
+
+
+
+
+
+///////////////////////////////////////////////////////////
+// BASIC TEAR - up to 2000 lead
+
+// -  on top ai tears inferiori -
+// -  mail promo/mese - creativita' e sviluppo mail promozionale da inciare alla lista
+// -  mail - creativita' e invio: GRATIS se il ciente ha generato 100 lead valide nei 30 giorni precedenti)
+// -  mail - i contatti sono considerati validi se il CRM conferma l'invio. la fattura verra' staccata a consuntivo.
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+// PREMIUM TEAR - up to 5000 lead
+
+// -  on top ai tears inferiori -
+// -  Sviluppo minisito: fino a 5 pagine (blog escluso)
+// -  landing page: design personalizzato fina a 10 sezioni oltre a sviluppo e messa online
+// -  promo code univoco: generazine di codice univoco per ogni lead.
+// -  tool di controllo e applicazione: attivazione del tool di controllo del codice univoco 
+
+
+
+
+
+/////////////////////////////////////////////////////
+// PLATINUM TEAR - on top ai tears inferiori - 10.000
+
+// -  on top ai tears inferiori -
+// -  1 minisito/una tantum: fino a 5 pagine
+// -  1 blog/una tantum: con CMS dedicato;
+// -  12 email/anno
+// -  CRM dedicato: accesso ai dati in conproprieta'.
+// -  landing page: design personalizzato fina a 10 sezioni oltre a sviluppo e messa online
+
+
+
+
+
+/////////////////////////////////////////////
+// listino - si possono realizzare in maniera indipendente alla sottoscrizione i seguenti servizi
+
+// - minisito: 1200 euro (up to 5 pages);
+// - blog: 700 euro (con CMS dedicato);
+// - landing page: 300 euro;
+// - promo code univoco: 400 euro;
+// - CRM dedicato: 400 euro;
+// - Email promo: copy + design + develope + invio: 100 euro;
+// - revisione exrta: 65 euro/ora;
+// - training/consultancy: cms, strategy, marketing: 100 euro/ora;
+
+
+
+
+/////////////////////////////////
+// MODALITA' DI COLLABORAZIONE
+
+// -  Brief: il cliente potra' prenotare il suo elaborato (eg: email promo, landing page, etc ..)direttamente dal pannello di amministrazione secondo le modalita' descritte in piattaforma. A seconda dell'elaborato richiesto possono variare le modalita' di collaborazione.
+// -  Revisione: per tutti gli elaborati e' prevista solo una revisione (brief del cliente -> design -> call di 30 min con il cliente per revisione -> invio).  
---
+++ contract.md
@@ -0,0 +1,73 @@
+# Contratto di Servizio Piqus Lead
+
+## Parti Coinvolte
+
+- **Il Titolare:** [Nome del Titolare], proprietario della landing page e della piattaforma.
+- **Il Cliente:** [Nome del Cliente], brand o servizio sponsorizzato, sottoscrittore del contratto.
+- **Gli Utenti:** Visitatori della landing page.
+- **Lead o Contatti:** Un utente che compila il form di contatto.
+- **Il Servizio:** 'Piqus Lead'.
+
+## Sottoscrizione del Contratto
+
+- Il seguente contratto regola il rapporto tra il titolare e il cliente in merito al servizio 'Piqus Lead'.
+- Il presente contratto è sottoscritto in fase di creazione dell'account 'Piqus Lead' attraverso la piattaforma raggiungibile dal sito 'lead.piqus.it'.
+- Attivando l'account, si considera sottoscritto il presente contratto.
+- Il presente contratto ha validità di 12 mesi e si rinnoverà automaticamente per mantenere continuità di servizio. Il cliente può interrompere la sua efficacia in qualsiasi momento inviando una mail a supporto@piqus.it o direttamente dalla piattaforma nel pannello di amministrazione.
+
+## Descrizione del Servizio 'Piqus Lead'
+
+Il servizio 'Piqus Lead' prevede la creazione di landing page o strumenti web finalizzati alla generazione di contatti (lead).
+
+Modello di servizio 'a performance': quanto più il cliente genera lead, tanto più ha diritto ai servizi descritti nei "tear" di riferimento come descritto nel seguente contratto. Il modello di servizio si divide in 4 livelli: 'Tear Light', 'Tear Basic', 'Tear Premium', 'Tear Platinum'.
+
+Non è possibile sottoscriversi a un 'tear' pagando, pertanto non sono richiesti dati di pagamento o fatturazione per la generazione dell'account. Il servizio 'Piqus Lead' si attiva semplicemente con la creazione di un account dal sito 'www.lead.piqus.it'. Al cliente viene attribuito il 'Tear: Light' di default. Il cliente verrà contattato direttamente per raccogliere le informazioni necessarie a creare il materiale proposto nel 'Tear: Light'.
+
+
+## LIGHT TEAR - fino a 500 lead
+
+- 1 landing page: design, sviluppo e messa online gratis della prima landing page promozionale. Versione standard (sezioni della pagina: header, promo message, form, footer)
+- 1 mail automatica: personalizzazione mail di risposta all'utente
+- 1 invio mail/anno: verso la lista contatti generata
+- Pannello admin: accesso al pannello di amministrazione
+- Account - supporto limitato alla consulenza creativa per i materiali in elenco
+
+## BASIC TEAR - fino a 2000 lead
+
+- In aggiunta a quanto specificato nei "Light Tear" -
+- Mail promo/mese: creatività e sviluppo mail promozionale da inviare alla lista
+- Mail: creatività e invio - GRATIS se il cliente ha generato 100 lead valide nei 30 giorni precedenti
+- Mail: i contatti sono considerati validi se il CRM conferma l'invio. La fattura verrà staccata a consuntivo.
+
+## PREMIUM TEAR - fino a 5000 lead
+
+- In aggiunta a quanto specificato nei "Basic Tear" -
+- Sviluppo minisito: fino a 5 pagine (blog escluso)
+- Landing page: design personalizzato fino a 10 sezioni oltre a sviluppo e messa online
+- Promo code univoco: generazione di codice univoco per ogni lead.
+- Tool di controllo e applicazione: attivazione del tool di controllo del codice univoco
+
+## PLATINUM TEAR - fino a 10.000 lead
+
+- In aggiunta a quanto specificato nei "Premium Tear" -
+- 1 minisito/una tantum: fino a 5 pagine
+- 1 blog/una tantum: con CMS dedicato;
+- 12 email/anno
+- CRM dedicato: accesso ai dati in coproprietà.
+- Landing page: design personalizzato fino a 10 sezioni oltre a sviluppo e messa online
+
+## Listino - Servizi Indipendenti alla Sottoscrizione
+
+- Minisito: 1200 euro (fino a 5 pagine)
+- Blog: 700 euro (con CMS dedicato)
+- Landing page: 300 euro
+- Promo code univoco: 400 euro
+- CRM dedicato: 400 euro
+- Email promo: copy + design + sviluppo + invio: 100 euro
+- Revisione extra: 65 euro/ora
+- Training/consultancy: CMS, strategy, marketing: 100 euro/ora
+
+## Modalità di Collaborazione
+
+- **Brief:** Il cliente potrà prenotare il suo elaborato (ad esempio, email promo, landing page, ecc.) direttamente dal pannello di amministrazione secondo le modalità descritte in piattaforma. A seconda dell'elaborato richiesto, possono variare le modalità di collaborazione.
+- **Revisione:** Per tutti gli elaborati è prevista solo una revisione (brief del cliente -> design -> call di 30 min con il cliente per revisione -> invio).
