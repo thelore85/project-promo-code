1424ed420dcd2efd16dd2c3e7e11d2c2b09407cc
TheLore85 <garofalo.digital@gmail.com>
2023-10-31 11:31:43 +0100

    feat: build version v1

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -8,6 +8,7 @@ import Footer from '../Components/Footer/Footer'
 import NavbarLogIn from '../Components/Navbar/NavbarLogIn'
 
 
+
 // layout template
 export default async function layout({children}) {
 
